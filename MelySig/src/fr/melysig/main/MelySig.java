410b10c33f865271f9bdcd679ae9d45ab1378bc7
Sébastien <Sbastien@10.154.131.211>
2014-01-31 12:14:55 +0100

    Mise en place de la configuration du projet, création du header - copyright générique.

---
+++ MelySig/src/fr/melysig/main/MelySig.java
@@ -0,0 +1,22 @@
+/*
+ * MelySig - Geolocalisation de points d'interets sur vos endroits favoris !
+ * MelySig est systeme d'information geographique qui animera vos decouvertes du monde :)
+ * Copyright 2014 - melysig.exia-nancy.com
+ * Auteurs : Pocecco Julien, Mougenel Gerold, Gaudenot Guillaume & Robert Sebastien.
+ */
+package fr.melysig.main;
+
+/**
+ *
+ * @author Sébastien
+ */
+public class MelySig {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* Mise en place */
+    }
+
+}
